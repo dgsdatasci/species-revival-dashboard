303649c11cd76396e008d3e06aeac4f383e8c905
Dylan Skalman <134750762+dgsdatasci@users.noreply.github.com>
2025-04-20 17:46:07 -0400

    Update iucn_api.py

--- app/app_utils/iucn_api.py
+++ app/app_utils/iucn_api.py
@@ -1,7 +1,7 @@
 import requests
 
 API_TOKEN = "p7RzHA8uG89cBhPFeEc8DJ4igu7D6bL6A8oJ"
-BASE_URL = "https://www.iucnredlist.org/"
+BASE_URL = "https://apiv3.iucnredlist.org/api/v3"
 
 def get_species_status(species_name):
     url = f"{BASE_URL}/species/{species_name}?token={API_TOKEN}"
